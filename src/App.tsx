e8398ff2898684719edbe17f5126ab4227d04fbf
Vinayak Mohanty <vinayak97696@gmail.com>
2025-03-07 22:15:36 +0530

    add VoiceCommandButton component; implement voice command functionality and integrate with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
 import { CartProvider } from "./context/CartContext";
+import { VoiceCommandButton } from "./components/VoiceCommandButton"; // Import the component
 
 import MainLayout from "./layouts/MainLayout";
 import OnboardingScreen from "./pages/onboarding/page";
@@ -18,10 +19,10 @@ import AddItems from "./pages/FarmerDasboard/AddItems";
 import FarmerProfile from "./pages/FarmerDasboard/FarmerProfile";
 import SearchFramersMarket from "./pages/SearchFramersMarket/SearchFramersMarket";
 
-export const BASE_URL = "https://toucan-driven-admittedly.ngrok-free.app";
+export const BASE_URL = "https://live-merely-drum.ngrok-free.app";
 
 export const accessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQwNDE4MDA3LCJpYXQiOjE3NDAyNDUyMDcsImp0aSI6IjAyODdkNzBjMmNhMjQ3NzFiN2Q4NDc5NzFjNWEzZjlmIiwidXNlcl9pZCI6NX0.eHOoZC-nRCIEIa1W7FqiXm3uRf6c0wDkHjEot6gbVOI";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQxNTM3ODM3LCJpYXQiOjE3NDEzNjUwMzcsImp0aSI6IjkyOTZhODEyZTIwNjRjYmM4ZmU3NmYyMTA5YWEwZGZmIiwidXNlcl9pZCI6MX0.EL6XtlBDcqdQRhy4ExgqlVKEAmCjbHY6Ne96Udcea-c";
 
 const queryClient = new QueryClient();
 
@@ -31,6 +32,8 @@ function App() {
       <Toaster />
       <CartProvider>
         <BrowserRouter>
+          {/* Voice command button that appears on all pages */}
+          <VoiceCommandButton />
           <Routes>
             <Route element={<MainLayout />}>
               {/* <Route path="/" element={<VintageCafeGame />} /> */}
